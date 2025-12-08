49d351f671ada928dc5e8f1096ac408c2ea57ebf
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2025-12-08 10:00:00 -0500

    Instancia y main de prueba

---
+++ eje01/main.cpp
@@ -0,0 +1,15 @@
+#include "Configuracion.h"
+
+int main() {
+    auto* cfg1 = Configuracion::getInstancia();
+    cfg1->mostrarConfiguracion();
+
+    cfg1->setIdioma("EN");
+    cfg1->setZonaHoraria("UTC");
+
+    auto* cfg2 = Configuracion::getInstancia();
+    cfg2->mostrarConfiguracion();
+
+    std::cout << "¿Es la misma instancia? "
+            << (cfg1 == cfg2 ? "Sí" : "No") << "\n";
+}
