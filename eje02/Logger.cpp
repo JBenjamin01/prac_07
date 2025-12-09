03d8ce39e844b992cc275e6d9c9d6d4335504328
dmolloc1 <dmolloc@unsa.edu.pe>
2025-12-08 22:12:49 -0500

    Se llama a logger desde diferentes funciones y se escribe en el mismo archivo

--- eje02/Logger.cpp
+++ eje02/Logger.cpp
@@ -21,4 +21,36 @@ public:
         return instancia;
     }
 
+    // metodo para registrar los mensajes en el logger
+    void log(const string& mensaje) {
+        time_t ahora = time(nullptr);
+        tm* t= localtime(&ahora);
+
+        char buffer[80];
+        strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", t);
+
+        archivo << "[" << buffer << "] " << mensaje << endl;
+    }
+    
+
+Logger* Logger::instancia = nullptr;
+
+void funcionA() {
+    Logger::getInstance()->log("Mensaje desde funcionA()");
+}
+
+void funcionB() {
+    Logger::getInstance()->log("Mensaje desde funcionB()");
+}
+
+int main() {
+    Logger::getInstance()->log("Inicio del programa");
+    funcionA();
+    funcionB();
+    Logger::getInstance()->log("Fin del programa");
+
+    cout << "Logs escritos en bitacora.log" << endl;
+    return 0;
+}
+
 };
